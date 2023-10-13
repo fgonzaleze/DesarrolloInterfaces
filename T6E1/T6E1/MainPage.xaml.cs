31b8f3322389d7f0e6bebe993ad83f38a4deb672
jgonzalez <jgonzaleze@iesnervion.es>
2023-10-13 10:11:46 +0200

    Ejercicio 3 completo

--- T6E1/T6E1/MainPage.xaml.cs
+++ T6E1/T6E1/MainPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿namespace T6E1
+﻿using Microsoft.UI.Xaml.Media;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace T6E1
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        Boolean btnExiste = false;
 
         public MainPage()
         {
@@ -11,8 +15,35 @@
 
         private void btn2Clicker(object sender, EventArgs e)
         {
+            if (btnExiste == false)
+            {
+                Button bt3 = new Button
+                {
 
-            
+                    Text = "Botón 3",
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    FontFamily = "Verdana",
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 16,
+                    HeightRequest = 70,
+                    WidthRequest = 200,
+                    BorderColor= Colors.Orange,
+                    BackgroundColor= Colors.Blue,
+                    Margin = 30,
+                    BorderWidth = 4,
+
+                };
+                btnExiste = true;
+                vlsBtns.Children.Add(bt3);
+                bt3.Clicked += new EventHandler(btn3Clicker);
+            }
+
         }
+        private void btn3Clicker(object sender, EventArgs e)
+        {
+            vlsBtns.Children.Remove(btn1);
+        }
+
     }
 }

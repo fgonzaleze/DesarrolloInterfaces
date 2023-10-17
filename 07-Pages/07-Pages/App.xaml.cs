943aaea25d28ceeaa63df1a5c77334aa938e01ea
jgonzalez <jgonzaleze@iesnervion.es>
2023-10-17 14:41:52 +0200

    Ejercicio1 t7

---
+++ 07-Pages/07-Pages/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _07_Pages.Views;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new PaginaTabbed();
+    }
+}
---
+++ 07-Pages/07-Pages/Views/Pagina3.xaml.cs
@@ -0,0 +1,9 @@
+namespace _07_Pages.Views;
+
+public partial class Pagina3 : ContentPage
+{
+	public Pagina3()
+	{
+		InitializeComponent();
+	}
+}

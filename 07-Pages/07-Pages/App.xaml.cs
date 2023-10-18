ac0d4f1b9cf50af7ace9eb580cd6f1636e11c701
jgonzalez <jgonzaleze@iesnervion.es>
2023-10-18 12:56:16 +0200

    Empezando ejercicio 2

--- 07-Pages/07-Pages/App.xaml.cs
+++ 07-Pages/07-Pages/App.xaml.cs
@@ -6,6 +6,6 @@ public partial class App : Application
     {
         InitializeComponent();
 
-        MainPage = new PaginaTabbed();
+        MainPage = new NavigationPage(new MainPage());
     }
 }
---
+++ 07-Pages/07-Pages/Views/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace _07_Pages.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}

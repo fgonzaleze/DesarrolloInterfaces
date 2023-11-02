4ebc4bdf7b28f076af4353cb0fe22c144bcde038
jgonzalez <jgonzaleze@iesnervion.es>
2023-11-02 13:51:41 +0100

    Ejercicio 4 completo

---
+++ E4T8/E4T8/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace E4T8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ E4T8/E4T8/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace E4T8
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
